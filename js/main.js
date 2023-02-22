bf0deac94d455b58386c464440bd3a3ea43e8bb5
DavideAtzo <atzo9477@gmail.com>
2023-02-22 16:51:49 +0100

    prova prendere valore dall'input

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
 'use strict';
 
-//l'utente potrà inserire gli input grazie al prompt
-const km = Number(prompt('inserire i km che vorresti fare'));
-const etaUtente = Number(prompt('inserire la tua eta'));
-console.log(km);
-console.log(etaUtente);
+//Dichiarazione variabili
+const inputElementkm = document.getElementById('km-da-percorrere');
+console.log(inputElementkm.value);
 
 
-// Calcolo biglietto
-const prezzoAlKm = 0.21;
-let prezzoBiglietto= km * prezzoAlKm;
+
+// // Calcolo biglietto
+// const prezzoAlKm = 0.21;
+// let prezzoBiglietto= km * prezzoAlKm;
 
 
-// variabili sconto del biglietto 
-const minorenne = 18;
-const over65 = 65;
+// // variabili sconto del biglietto 
+// const minorenne = 18;
+// const over65 = 65;
 
-if(etaUtente < 18)
-prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.2;
+// if(etaUtente < 18)
+// prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.2;
 
-else if(etaUtente >= 65)
-prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.4;
+// else if(etaUtente >= 65)
+// prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.4;
 
 
 // Inerisco i dati negli id per la visualizzazione degli input
-document.getElementById('km').innerHTML = km;
-document.getElementById('eta-utente').innerHTML = etaUtente;
-document.getElementById('prezzo-biglietto').innerHTML = prezzoBiglietto.toFixed(2);
-console.log(prezzoBiglietto);
+// document.getElementById('km').innerHTML = km;
+// // document.getElementById('eta-utente').innerHTML = etaUtente;
+// document.getElementById('prezzo-biglietto').innerHTML = prezzoBiglietto.toFixed(2);
+// console.log(prezzoBiglietto);
