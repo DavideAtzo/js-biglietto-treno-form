ba83dbb192a74c267dfb12e3008b985b5afc3459
DavideAtzo <atzo9477@gmail.com>
2023-02-22 15:05:00 +0100

    calcolo biglietto passeggero

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//l'utente potrà inserire gli input grazie al prompt
+const km = Number(prompt('inserire i km che vorresti fare'))
+const etaUtente = Number(prompt('inserire la tua eta'))
+console.log(km);
+console.log(etaUtente);
+
+
+// Calcolo biglietto
+const prezzoAlKm = 0.21;
+let prezzoBiglietto= km * prezzoAlKm;
+console.log(prezzoBiglietto);
+
+// variabili sconto del biglietto 
+const minorenne = 18;
+const over65 = 65;
+
+if(etaUtente < 18)
+prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.2;
+
+else if(etaUtente >= 65)
+prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.4;
+
