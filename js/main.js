0ca6217094dfd9cee76caf6e9f084c99f389c677
DavideAtzo <atzo9477@gmail.com>
2023-02-22 15:19:49 +0100

    html base + visualizzazione input

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
 //l'utente potrà inserire gli input grazie al prompt
-const km = Number(prompt('inserire i km che vorresti fare'))
-const etaUtente = Number(prompt('inserire la tua eta'))
+const km = Number(prompt('inserire i km che vorresti fare'));
+const etaUtente = Number(prompt('inserire la tua eta'));
 console.log(km);
 console.log(etaUtente);
 
@@ -15,6 +15,9 @@ console.log(prezzoBiglietto);
 // variabili sconto del biglietto 
 const minorenne = 18;
 const over65 = 65;
+console.log(minorenne);
+console.log(over65);
+
 
 if(etaUtente < 18)
 prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.2;
@@ -22,3 +25,8 @@ prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.2;
 else if(etaUtente >= 65)
 prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.4;
 
+
+// Inerisco i dati negli id per la visualizzazione degli input
+document.getElementById('km').innerHTML = km;
+document.getElementById('eta-utente').innerHTML = etaUtente;
+document.getElementById('prezzo-biglietto').innerHTML = prezzoBiglietto.toFixed(2);
